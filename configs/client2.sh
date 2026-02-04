b1fb2f351ed79e90c6c7a08aafe5fb5b3b1650d4
Kunal1794 <54974430+Kunal1794@users.noreply.github.com>
2026-02-04 13:39:09 +0530

    Update client2.sh

--- configs/client2.sh
+++ configs/client2.sh
@@ -1,7 +1,7 @@
 ip link add link eth1 name eth1.10 type vlan id 10
 ip link set eth1.10 up
-export HTTP_PROXY="http://inban1b-proxy.apac.nsn-net.net:8080"
-export HTTPS_PROXY="http://inban1b-proxy.apac.nsn-net.net:8080"
+#export HTTP_PROXY="x.x.x.x:x"
+#export HTTPS_PROXY="x.x.x.x:x"
 echo -e "PermitRootLogin yes\nPubkeyAuthentication yes\nAuthorizedKeysFile .ssh/authorized_keys\nPasswordAuthentication yes\nPermitEmptyPasswords yes\nAllowTcpForwarding no\nGatewayPorts no\nX11Forwarding no\nSubsystem sftp /usr/lib/ssh/sftp-server" | tee /etc/ssh/sshd_config > /dev/null
 echo "root:Nokia@123" | chpasswd
 apk add openssh
